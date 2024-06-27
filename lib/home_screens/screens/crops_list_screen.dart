c7d074c71439a0055057dc9fac1f0c5198be8053
Damarists <u202119605@upc.edu.pe>
2024-06-27 11:26:38 -0500

    Last Changes

--- lib/home_screens/screens/crops_list_screen.dart
+++ lib/home_screens/screens/crops_list_screen.dart
@@ -1,10 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:ayni_flutter_app/feature_crops/screens/crop_details_screen.dart';
-import 'package:ayni_flutter_app/finance_screens/screens/transaction_panels.dart';
-import 'package:ayni_flutter_app/home_screens/screens/products_list_screen.dart';
-import 'package:ayni_flutter_app/feature_orders/screens/sales_list_screen.dart';
-import 'package:ayni_flutter_app/shared/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:ayni_flutter_app/home_screens/models/products.dart';
 import 'package:ayni_flutter_app/home_screens/screens/crops_add_screen.dart';
@@ -40,7 +35,7 @@ class _CropsListScreenState extends State<CropsListScreen> {
   }
 
   void _startShuffleTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 10), (Timer timer) {
+    _timer = Timer.periodic(Duration(seconds: 10), (Timer timer) {
       _shuffleProducts();
     });
   }
@@ -50,17 +45,6 @@ class _CropsListScreenState extends State<CropsListScreen> {
     setState(() {
       _products = products;
       _shuffledProducts = List.of(products);
-      _isLoading = false;
-    });
-  }
-
-  void _onQuery(String query) async {
-    setState(() {
-      _isLoading = true;
-    });
-    List<Products> products = await _productsService.getByName(query);
-    setState(() {
-      _products = products;
       _isLoading = false;
     });
   }
@@ -88,16 +72,21 @@ class _CropsListScreenState extends State<CropsListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Crops')),
+        title: Center(child: Text('Crops')),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Filter'),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              controller: _searchController,  
-              onSubmitted: _onQuery,
-              decoration: const InputDecoration(
+              controller: _searchController,
+              decoration: InputDecoration(
                 labelText: "Search",
                 hintText: "Search",
                 suffixIcon: Icon(Icons.search),
@@ -129,101 +118,80 @@ class _CropsListScreenState extends State<CropsListScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Align(
-          /*const Align(
             alignment: Alignment.centerLeft,
             child: Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Text('Hot Deals', style: TextStyle(fontSize: 24)),
             ),
           ),
-          SizedBox(
+          Container(
             height: 200,
             child: _isLoading
-                ? const Center(child: CircularProgressIndicator())
+                ? Center(child: CircularProgressIndicator())
                 : ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: _shuffledProducts.length,
                     itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CropDetailsScreen(product: _shuffledProducts[index]),
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image.network(
+                                _shuffledProducts[index].imageUrl,
+                                width: 150,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          );
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10.0),
-                                child: Image.network(
-                                  _shuffledProducts[index].imageUrl,
-                                  width: 150,
-                                  height: 150,
-                                  fit: BoxFit.cover,
-                                ),
+                            SizedBox(height: 9.0),
+                            Text(
+                              _shuffledProducts[index].name,
+                              style: TextStyle(
+                                fontSize: 16,
                               ),
-                              const SizedBox(height: 9.0),
-                              Text(
-                                _shuffledProducts[index].name,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       );
                     },
                   ),
-          ),*/
+          ),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Text('All my products', style: TextStyle(fontSize: 24)),
                 ),
                 Expanded(
                   child: GridView.builder(
                     padding: const EdgeInsets.all(8.0),
                     itemCount: _products.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
                       crossAxisSpacing: 10,
                       mainAxisSpacing: 10,
                       childAspectRatio: 0.6,
                     ),
                     itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                        builder: (context) => CropDetailsScreen(product: _products[index]),
-                      ),
-                    );
-                        },
-                        child: Column(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                _products[index].imageUrl,
-                                width: double.infinity,
-                                height: 150,
-                                fit: BoxFit.cover,
-                              ),
+                      return Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              _products[index].imageUrl,
+                              width: double.infinity,
+                              height: 150,
+                              fit: BoxFit.cover,
                             ),
-                            const SizedBox(height: 9.0),
-                            Text(_products[index].name,
-                                style: const TextStyle(fontSize: 16)),
-                          ],
-                        ),
+                          ),
+                          SizedBox(height: 9.0),
+                          Text(_products[index].name,
+                              style: TextStyle(fontSize: 16)),
+                        ],
                       );
                     },
                   ),
@@ -266,36 +234,7 @@ class _CropsListScreenState extends State<CropsListScreen> {
             ),
           );
         },
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.add, color: Colors.white),
       ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            height: 1.0, // Altura de la línea negra
-            color: Colors.white, // Color de la línea negra
-          ),
-BottomNavBar(currentIndex: 1, 
-        onTap: (index){
-          switch(index){
-            case 0:
-              Navigator.push(context, SlideTransitionPageRoute(page: ProductsListScreen()));
-              break;
-            case 1:
-              Navigator.push(context, SlideTransitionPageRoute(page: CropsListScreen()));
-              break;
-            case 2:
-              Navigator.push(context, SlideTransitionPageRoute(page: const SalesListScreen()));
-              break;
-            case 3:
-              Navigator.push(context, SlideTransitionPageRoute(page: TransactionListScreen2()));
-              break;
-          }
-        }),
-        ]
-      )
-      
     );
   }
 }
--- lib/home_screens/screens/products_list_screen.dart
+++ lib/home_screens/screens/products_list_screen.dart
@@ -1,8 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:ayni_flutter_app/feature_orders/screens/sales_list_screen.dart';
-import 'package:ayni_flutter_app/finance_screens/screens/transaction_panels.dart';
-import 'package:ayni_flutter_app/shared/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:ayni_flutter_app/home_screens/screens/crops_list_screen.dart';
 import 'package:ayni_flutter_app/home_screens/services/products_service.dart';
@@ -38,7 +35,7 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
   }
 
   void _startShuffleTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 10), (Timer timer) {
+    _timer = Timer.periodic(Duration(seconds: 10), (Timer timer) {
       _shuffleProducts();
     });
   }
@@ -75,7 +72,16 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
@@ -116,62 +122,6 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
                                       borderRadius: BorderRadius.circular(10.0),
                                     ),
                                     hintText: 'Search',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('Latest Crops',
-                          style: TextStyle(fontSize: 24)),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CropsListScreen()),
-                          );
-                        },
-                        child: const Text('See All ->',
-                            style: TextStyle(color: Colors.red)),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 200,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: _shuffledProducts.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.network(
-                                  _shuffledProducts[index].imageUrl,
-                                  width: 100,
-                                  height: 150,
-                                  fit: BoxFit.cover),
-                            ),
-                            Positioned(
-                              top: 8.0,
-                              left: 8.0,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8.0, vertical: 4.0),
-                                child: Text(
-                                  _shuffledProducts[index].name,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
                                   ),
                                 ),
                               ),
@@ -291,41 +241,6 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
                 ],
               ),
             ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            height: 1.0, // Altura de la línea negra
-            color: Colors.white, // Color de la línea negra
-          ),
-          BottomNavBar(
-              currentIndex: 0,
-              onTap: (index) {
-                switch (index) {
-                  case 0:
-                    Navigator.push(context,
-                        SlideTransitionPageRoute(page: ProductsListScreen()));
-                    break;
-                  case 1:
-                    Navigator.push(context,
-                        SlideTransitionPageRoute(page: CropsListScreen()));
-                    break;
-                  case 2:
-                    Navigator.push(
-                        context,
-                        SlideTransitionPageRoute(
-                            page: const SalesListScreen()));
-                    break;
-                  case 3:
-                    Navigator.push(
-                        context,
-                        SlideTransitionPageRoute(
-                            page: TransactionListScreen2()));
-                    break;
-                }
-              }),
-        ],
-      ),
     );
   }
 
